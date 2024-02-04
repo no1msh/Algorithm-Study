fce01a5a151fb6ed63db7aead47707a16e712639
Bandal <no1msh1217@gmail.com>
2024-02-04 22:01:54 +0900

    feat: 선택 정렬 구현

---
+++ src/main/kotlin/sort/SelectionSort.kt
@@ -0,0 +1,33 @@
+package sort
+
+object SelectionSort : Sort {
+    /**
+     * 선택정렬은 비교 기반 정렬 알고리즘으로 배열 전체를 돌며 최소값을 찾아 오름차순으로 정렬합니다.
+     *
+     * 매번 배열 전체를 순회하며 하는 것이 아닌 n 번째 값을 정렬하려면 n - 1번 순회하게 합니다.
+     *
+     * (n - 1) + (n - 2) + ... + 2 + 1 = n(n - 1) / 2 이므로 시간 복잡도는 O(n²)입니다.
+     *
+     * 다른 정렬 알고리즘에 비해 비교적 느린 편이지만, 별도의 메모리 공간을 필요로 하진 않는다는 장점이 있습니다.
+     *
+     * 최소값을 찾으면 검색하던 인덱스 값을 교환을 하는 방식을 사용하여 불안정 정렬입니다.
+     **/
+    override fun sort(target: IntArray) {
+        var indexForSort = 0
+        while (indexForSort < target.size) {
+            var minIndex = indexForSort
+
+            for (index in indexForSort..target.lastIndex) {
+                if (target[minIndex] > target[index]) {
+                    minIndex = index
+                }
+            }
+
+            val temp = target[indexForSort]
+            target[indexForSort] = target[minIndex]
+            target[minIndex] = temp
+
+            indexForSort++
+        }
+    }
+}
---
+++ src/test/kotlin/sort/SelectionSortTest.kt
@@ -0,0 +1,31 @@
+package sort
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class SelectionSortTest {
+    @Test
+    fun `선택 정렬 알고리즘을 이용하여 정렬할 수 있다`() {
+        // given
+        val target = intArrayOf(3, 2, 5, 4, 1)
+
+        // when
+        SelectionSort.sort(target)
+
+        // then
+        Assertions.assertThat(target).isEqualTo(intArrayOf(1, 2, 3, 4, 5))
+    }
+
+    @Test
+    fun `선택 정렬 알고리즘을 이용하여 10만개의 원소를 가진 배열을 정렬할 수 있다`() {
+        // given
+        val expect = IntArray(100_000) { it }
+        val target = expect.reversed().toIntArray()
+
+        // when
+        SelectionSort.sort(target)
+
+        // then
+        Assertions.assertThat(target).isEqualTo(expect)
+    }
+}
